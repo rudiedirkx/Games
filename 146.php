924d913b9819677be7380133962c129ec5c002fe
Rudie Dirkx <github@hotblocks.nl>
2013-06-11 03:14:56 +0200

    slither+

--- 146.php
+++ 146.php
@@ -1,519 +1,13 @@
 <?php
 // SLITHER
 
-$g_arrBoards = array(
-	'easy'	=> array(
-		100 => array( // 0
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'    1',
-				' 3',
-				' 3  1',
-			),
-		),
-		101 => array( // 1
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'  0',
-				'  3',
-				'1  2',
-				' 23',
-			),
-		),
-		array( // 2
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'01010',
-				' 3  2',
-				'2 3',
-				'1 1 1',
-				'  3',
-			),
-		),
-		array( // 3
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 313',
-				'  2',
-				' 202',
-				'  2',
-				'3 1 3',
-			),
-		),
-		array( // 4
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 231',
-				'  20',
-				' 2',
-				'1 2 1',
-				'0   0',
-			),
-		),
-		array( // 5
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'',
-				' 222',
-				'2 3 2',
-				'  2',
-				'31  1',
-			),
-		),
-		array( // 6
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 33 0',
-				'1 1 2',
-				'2 322',
-				'3 2',
-				' 1  3',
-			),
-		),
-		array( // 7
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'2332',
-				'   12',
-				'23 3',
-				'  022',
-				' 333',
-			),
-		),
-		array( // 8
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 122',
-				'3',
-				'3021',
-				'32311',
-				' 2 2',
-			),
-		),
-		array( // 9
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'3 3',
-				'  0 1',
-				' 23',
-				'322 2',
-				' 2',
-			),
-		),
-		array( // 10
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 1',
-				'  2 2',
-				'    2',
-				'12203',
-				' 0',
-			),
-		),
-		array( // 11
-			'size'	=> array(6,6),
-			'board'	=> array(
-				'0 1 22',
-				' 2  01',
-				'1 2 22',
-				'   0',
-				'202 0',
-				'212  0',
-			),
-		),
-		array( // 12
-			'size'	=> array(6,6),
-			'board'	=> array(
-				'1   32',
-				'3 0 13',
-				'    2',
-				'2 11',
-				'  1',
-				' 3 23',
-			),
-		),
-		array( // 13
-			'size'	=> array(6,6),
-			'board'	=> array(
-				' 22 0',
-				'2 1',
-				'20  13',
-				'  0',
-				'  1 1',
-				'  212',
-			),
-		),
-		array( // 14
-			'size'	=> array(6,6),
-			'board'	=> array(
-				'  22',
-				'2 00 2',
-				'2 00 2',
-				'  11',
-				'  11',
-				' 3  3',
-			),
-		),
-		array( // 15
-			'size'	=> array(6,6),
-			'board'	=> array(
-				' 2223',
-				'     0',
-				' 2 2',
-				'    2',
-				' 32 1',
-				'0  22',
-			),
-		),
-		array( // 16
-			'size'	=> array(7,7),
-			'board'	=> array(
-				'   3 0',
-				'  20 20',
-				' 2',
-				'3 101 3',
-				'   2 1',
-				'',
-				'0 321 0',
-			),
-		),
-		array( // 17
-			'size'	=> array(7,7),
-			'board'	=> array(
-				' 22  0',
-				' 11  0',
-				'201',
-				'    1 3',
-				'2 10',
-				'   1 1',
-				'   2 2',
-			),
-		),
-		array( // 18
-			'size'	=> array(7,7),
-			'board'	=> array(
-				'    3',
-				' 2 11',
-				'3110',
-				'     23',
-				'  21',
-				'  11 0',
-				'  22 0',
-			),
-		),
-		array( // 19
-			'size'	=> array(7,7),
-			'board'	=> array(
-				'  22',
-				'21 1 0',
-				'2101',
-				'  1 12',
-				' 2   2',
-				'  0',
-				' 21 3',
-			),
-		),
-		array( // 20
-			'size'	=> array(7,7),
-			'board'	=> array(
-				'2 2',
-				'101',
-				'21  0',
-				'  12',
-				'  21 12',
-				' 0   0',
-				'    212',
-			),
-		),
-		array( // 21
-			'size'	=> array(10,10),
-			'board'	=> array(
-				'212 22   3',
-				'1 0212 211',
-				'2 2    102',
-				'3 1121201',
-				'  11 2 112',
-				'3211 3 3 2',
-				'   2     3',
-				' 3 2   3',
-				' 22021102',
-				'   3 2112',
-			),
-		),
-		array( // 22
-			'size'	=> array(10,10),
-			'board'	=> array(
-				'    22',
-				'',
-				'    11',
-				' 111  111',
-				'2        2',
-				'  1 00 1',
-				'  1 00 1',
-				'  21  12',
-				'    11',
-				'    22',
-			),
-		),
-		array( // 23
-			'size'	=> array(10,10),
-			'board'	=> array(
-				'',
-				'0 2 12',
-				' 2 0',
-				' 2     3',
-				'     0',
-				'  21101',
-				'     1  1',
-				'     1   2',
-				'    3  2',
-			),
-		),
-		array( // 24
-			'size'	=> array(10,10),
-			'board'	=> array(
-				'22',
-				'21',
-				'     00',
-				'  2',
-				' 2 2',
-				'  1 1 1',
-				'0 10001',
-				'  2 1 1 1',
-				'        1',
-				'         3',
-			),
-		),
-		array( // 25
-			'size'	=> array(10,10),
-			'board'	=> array(
-				'    3 13',
-				'      1',
-				'    2',
-				' 211  1',
-				'     1 12',
-				' 2001 21 3',
-				'  2 1',
-				'   11',
-				'   1  23',
-				' 32 12',
-			),
-		),
-	),
-	'normal' => array(
-		201 => array( // 1
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'3 31',
-				'    3',
-				'  303',
-				' 2',
-				'22 1',
-			),
-		),
-		array( // 2
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 333',
-				' 202',
-				'   2',
-				'2 23',
-				'  2',
-			),
-		),
-		array( // 3
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'3 33',
-				'  022',
-				' 32 2',
-				'  1 2',
-				' 2233',
-			),
-		),
-		array( // 4
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 233',
-				' 120',
-				'212 3',
-				'',
-				'   12',
-			),
-		),
-		array( // 5
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'2 2',
-				'2 12',
-				'2',
-				'120',
-				' 33 3',
-			),
-		),
-		array( // 6
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'   33',
-				' 32 2',
-				'    3',
-				'   03',
-				'3233',
-			),
-		),
-		array( // 7
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'3  1',
-				'    3',
-				'3  13',
-				'2   2',
-				'02  0',
-			),
-		),
-		array( // 8
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'02',
-				'2  2',
-				'3 323',
-				'322',
-				' 1 2',
-			),
-		),
-		array( // 9
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'   3',
-				'131',
-				' 30',
-				'  3',
-				' 2 2',
-			),
-		),
-		array( // 10
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'322',
-				'   3',
-				' 132',
-				'3 1 2',
-				' 12 0',
-			),
-		),
-	),
-	'hard' => array(
-		301 => array( // 1
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 23 3',
-				'  12',
-				'222 1',
-				'33 3',
-				'    2',
-			),
-		),
-		array( // 2
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 3  3',
-				'2 1',
-				'213 3',
-				' 3',
-				'  2 3',
-			),
-		),
-		array( // 3
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'2',
-				' 22',
-				'1 2',
-				'2  3',
-				'   13',
-			),
-		),
-		array( // 4
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'3 2 2',
-				'   3',
-				' 02',
-				'   3',
-				'3 23',
-			),
-		),
-		array( // 5
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'  2',
-				'23 32',
-				'31  2',
-				'   23',
-				' 2',
-			),
-		),
-		array( // 6
-			'size'	=> array(5,5),
-			'board'	=> array(
-				'   3 3',
-				'1212',
-				'  2',
-				'23 3',
-				'221',
-			),
-		),
-		array( // 7
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 1',
-				'  3 2',
-				'    2',
-				'12203',
-				' 0',
-			),
-		),
-		array( // 8
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 33',
-				' 0  2',
-				' 323',
-				' 2',
-				'3  2',
-			),
-		),
-		array( // 9
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 31 3',
-				' 3 2',
-				'  2 3',
-				'32 2',
-				' 2  3',
-			),
-		),
-		array( // 10
-			'size'	=> array(5,5),
-			'board'	=> array(
-				' 3',
-				' 03 2',
-				'3  2',
-				'   2',
-				'33  3',
-			),
-		),
-	),
-);
-
+$g_arrBoards = require '146_levels.php';
 
 $done = isset($_COOKIE['g146']) ? array_map('intval', explode(',', $_COOKIE['g146'])) : array();
-
 
 $iGame = isset($_GET['lvl']) && ( isset($g_arrBoards['easy'][$_GET['lvl']]) || isset($g_arrBoards['normal'][$_GET['lvl']]) || isset($g_arrBoards['hard'][$_GET['lvl']]) ) ? (int)$_GET['lvl'] : 101;
 $szDifficulty = !isset($_GET['lvl'])  ? 'easy' : ( isset($g_arrBoards['normal'][$_GET['lvl']]) ? 'normal' : ( isset($g_arrBoards['hard'][$_GET['lvl']]) ? 'hard' : 'easy' ) );
 $arrGame = (array)$g_arrBoards[$szDifficulty][$iGame];
-
 
 $b = 5;		// border width
 $t = 26;	// td 'size'
@@ -829,10 +323,14 @@ function checkDone(btn) {
 		alert("That's not it... One link. White numbers. A slithering snake.");
 	}
 }
+
+function xor(a, b) {
+	return (a || b) && !(a && b);
+}
 </script>
 </head>
 
-<body>
+<body onload="init()">
 
 <form>
 	<select id="levelselect0r">
@@ -867,42 +365,39 @@ for ( $i=0; $i<$r; $i++ )
 </table>
 </div>
 
-<div id="red" style="position: absolute; width: 6px; height: 6px; background: red;"></div>
+<div id="red" style="position: absolute; width: 6px; height: 6px; margin: -3px 0 0 -3px; background: red;"></div>
 
 <script>
 var evType = 'ontouchstart' in document.documentElement ? 'touchstart' : 'mousedown',
-	g_szTable = $('slither_div').innerHTML;
+	$slither = $('slither');
 
 function init() {
-	$('slither').on(evType, '.horbor, .verbor', function(e) {
-		this.attr('not') == '1' || clickBorder(this);
-		$('red').css(e.pageXY.subtract({x:3, y:3}).toCSS());
-	}).on('contextmenu', '.horbor, .verbor', function(e) {
-		e.preventDefault();
+	$slither.on(evType, function(e) {
+		if ( e.rightClick ) return true;
 
-		if ( this.attr('on') == '1' ) {
-			clickBorder(this);
-			rightClickBorder(this);
-			return;
+		var tw = $slither.getBoundingClientRect().width,
+			bw = $slither.getElement('.verbor').getBoundingClientRect().width,
+			ss = (tw-bw) / 5;
+
+		var col = Math.floor(e.subjectXY.x / (tw / 16)),
+			row = Math.floor(e.subjectXY.y / (tw / 16));
+
+		if ( xor(col % 3 == 0, row % 3 == 0) ) {
+			$('red').css(e.pageXY.toCSS());
+
+			if ( col%3 == 0 ) {
+				col /= 3;
+				var row = Math.floor(e.subjectXY.y / ss);
+				clickBorder(this.rows[row*2+1].cells[col*2]);
+			}
+			else {
+				row /= 3;
+				var col = Math.floor(e.subjectXY.x / ss);
+				clickBorder(this.rows[row*2].cells[col*2+1]);
+			}
 		}
-
-		rightClickBorder(this);
 	});
 }
-
-document.on('keydown', function(e) {
-	if ( 67 == e.key ) {
-		$('slither_div').setHTML(g_szTable);
-		init();
-	}
-});
-
-init();
-
-$('slither_div').on('contextmenu', function(e) {
-	e.preventDefault();
-});
-
 
 $('levelselect0r').on('change', function(e) {
 	if ( this.value && <?= $iGame ?> != this.value ) {
