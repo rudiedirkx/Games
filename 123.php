3422121125abab769317d6ad3a9bf89505c95678
Rudie Dirkx <github@hotblocks.nl>
2015-04-12 13:55:59 +0200

    stepping stones+

--- 123.php
+++ 123.php
@@ -3,22 +3,20 @@
 
 session_start();
 
-require_once("connect.php");
+// require_once("connect.php");
 define( "S_NAME", "st_user" );
-
-require_once('inc.cls.json.php');
 
 require_once('steppingstones_levels.php');
 $g_iFirstLevel = key($g_arrLevels);
 
 
-if ( isset($_POST['newlevel'], $_POST['level']) ) {
-	if ( !isset($g_arrLevels[$_POST['level']]) ) {
-		exit(json::encode(array('error' => 'Invalid level!')));
+if ( isset($_GET['newlevel'], $_GET['level']) ) {
+	if ( !isset($g_arrLevels[$_GET['level']]) ) {
+		exit(json_encode(array('error' => 'Invalid level!')));
 	}
-	$arrLevel = $g_arrLevels[$_POST['level']];
-	$arrLevel['level'] = $_POST['level'];
-	exit(json::encode($arrLevel));
+	$arrLevel = $g_arrLevels[$_GET['level']];
+	$arrLevel['level'] = $_GET['level'];
+	exit(json_encode($arrLevel));
 }
 
 else if ( isset($_POST['new_name']) ) {
@@ -40,8 +38,9 @@ else if ( isset($_POST['name'], $_POST['level'], $_POST['jumps']) ) {
 			}
 		}
 	}
+
 	$iStartStones = $iStones;
-	$arrJumps = explode(',', $_POST['jumps']);
+	$arrJumps = (array) $_POST['jumps'];
 	foreach ( $arrJumps AS $szJump ) {
 		$x = explode(':', $szJump);
 		$dir = array_pop($x);
@@ -60,28 +59,28 @@ else if ( isset($_POST['name'], $_POST['level'], $_POST['jumps']) ) {
 			$iStones--;
 		}
 		else {
-			exit('Invalid jump when '.$iStones.' stones left.');
+			exit('Invalid jump when ' . $iStones . ' stones left.');
 		}
 		unset($nowField, $overField, $toField);
 	}
+
 	if ( $iStones < $iStartStones/2 ) {
-		mysql_query("INSERT INTO steppingstones (name, score, level, utc) VALUES ('".addslashes($_POST['name'])."', ".$iStones.", '".addslashes($_POST['level'])."', ".time().")");
-		exit('Game saved ('.$iStones.' stones left)!');
+		// mysql_query("INSERT INTO steppingstones (name, score, level, utc) VALUES ('".addslashes($_POST['name'])."', ".$iStones.", '".addslashes($_POST['level'])."', ".time().")");
+		exit('Game saved (' . $iStones . ' stones left)!');
 	}
-	exit($iStones.' stones left. Not good enough!');
+
+	exit($iStones . ' stones left. Not good enough!');
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!doctype html>
+<html>
 
 <head>
 <title>STEPPING STONES</title>
 <link rel="stylesheet" type="text/css" href="123.css" />
-<script type="text/javascript" src="/js/mootools_1_11.js"></script>
-<script type="text/javascript">
-<!--//
+<script src="js/rjs-custom.js"></script>
+<script>
 function SteppingStones( f_level ) {
 	this.m_szName = 'Anonymous';
 	this.m_iLevel = 0;
@@ -92,12 +91,12 @@ function SteppingStones( f_level ) {
 
 	this.loadLevel(f_level);
 }
-
-SteppingStones.prototype = {
+r.extend(SteppingStones, {
 	selectStone : function( f_objTD ) {
 		if ( f_objTD.available && f_objTD.stone ) {
 			// Unselect current stone
 			$('stones_tbody').rows[this.m_arrJumper[1]].cells[this.m_arrJumper[0]].className = 'stone';
+
 			// Select new stone
 			f_objTD.className = 'jumper';
 			this.m_arrJumper = [f_objTD.cellIndex, f_objTD.parentNode.sectionRowIndex];
@@ -112,19 +111,24 @@ SteppingStones.prototype = {
 		var toField = $('stones_tbody').rows[toFieldC[1]].cells[toFieldC[0]];
 		if ( overField.stone && !toField.stone && toField.available ) {
 			var nowField = $('stones_tbody').rows[this.m_arrJumper[1]].cells[this.m_arrJumper[0]];
-			var nowFieldC = this.m_arrJumper.clone();
+			var nowFieldC = r.copy(this.m_arrJumper);
+
 			// nowField is stoneless
 			nowField.className = 'no-stone';
 			nowField.stone = false;
+
 			// overField is stoneless
 			overField.stone = false;
 			overField.className = 'no-stone';
+
 			// toField is stone and jumper
 			this.m_arrJumper = toFieldC;
 			toField.stone = true;
 			toField.className = 'jumper';
+
 			this.m_iStones--;
 			$('stats_stonesleft').innerHTML = this.m_iStones;
+
 			// Save order
 			this.addToStack(nowFieldC, f_szDir);
 			if ( 1 == this.m_iStones ) {
@@ -135,21 +139,27 @@ SteppingStones.prototype = {
 	},
 
 	saveGame : function() {
-		if ( this.m_bGameOver || 1 >= this.m_arrStack.length ) { return false; }
+		if ( this.m_bGameOver || this.m_arrStack.length <= 1 ) return;
+
 		this.m_bGameOver = true;
-		var szName = prompt('What\'s your name?', this.m_szName);
+		var szName = prompt("What's your name?", this.m_szName);
 		if ( !szName ) {
 			return false;
 		}
+
 		this.m_szName = szName;
-		$('your_name').innerHTML = szName;
+		$('your_name').setText(szName);
+
 		var self = this;
-		new Ajax('?', {
-			data : 'name=' + encodeURIComponent(szName) + '&level=' + this.m_iLevel + '&jumps=' + this.m_arrStack.join(','),
-			onComplete : function(t) {
-				self.SaveMessage(t);
-			}
-		}).request();
+		var data = {
+			"name": szName,
+			"level": this.m_iLevel,
+			"jumps": this.m_arrStack
+		};
+		r.post('', r.serialize(data)).on('done', function(e, t) {
+			self.SaveMessage(t);
+		});
+
 		this.m_arrStack = [];
 		return false;
 	},
@@ -159,7 +169,7 @@ SteppingStones.prototype = {
 	},
 
 	coordsByDir : function( f_arrStart, f_szDir ) {
-		var to = f_arrStart.clone();
+		var to = r.copy(f_arrStart);
 		switch ( f_szDir.substr(0, 1) ) {
 			case 'u': to[1]--; break;
 			case 'd': to[1]++; break;
@@ -171,63 +181,57 @@ SteppingStones.prototype = {
 
 	loadLevel : function( f_iLevel ) {
 		var self = this;
-		new Ajax('?', {
-			data : 'newlevel=1&level=' + f_iLevel,
-			onComplete : function(t) {
-				try {
-					var rv = eval( "(" + t + ")" );
-				} catch (e) {
-					alert('Response error: '+t);
-					return;
-				}
-				if ( rv.error ) {
-					alert(rv.error);
-					return;
-				}
-				// Save level
-				self.m_bGameOver = false;
-				self.m_iLevel = rv.level;
-				$('stats_level').innerHTML = rv.level;
-				self.m_arrJumper = rv.jumper;
-				self.m_arrStack = [];
-				// empty current map
-				while ( 0 < $('stones_tbody').childNodes.length ) {
-					$('stones_tbody').removeChild($('stones_tbody').firstChild);
-				}
-				// save map
-				self.m_iStones = 0;
-				$A(rv.map).each(function(row, y) {
-					var nr = $('stones_tbody').insertRow($('stones_tbody').rows.length);
-					for ( var x=0; x<row.length; x++ ) {
-						var nc = nr.insertCell(nr.cells.length);
-						nc.innerHTML = '';
-						if ( 'o' == row.substr(x, 1) ) {
-							nc.className = 'no-stone';
-							nc.available = true;
-						}
-						else if ( 's' == row.substr(x, 1) ) {
-							nc.className = 'stone';
-							nc.available = true;
-							nc.stone = true;
-							self.m_iStones++;
-						}
-						else {
-							nc.className = '';
-						}
-					}
-				});
-				$('stats_stonesleft').innerHTML = self.m_iStones;
-				// show jumper
-				$('stones_tbody').rows[rv.jumper[1]].cells[rv.jumper[0]].className = 'jumper';
+		r.get('?newlevel=1&level=' + f_iLevel).on('done', function(e, rv) {
+			if ( rv.error ) {
+				alert(rv.error);
+				return;
 			}
-		}).request();
+
+			// Save level
+			self.m_bGameOver = false;
+			self.m_iLevel = rv.level;
+			$('stats_level').innerHTML = rv.level;
+			self.m_arrJumper = rv.jumper;
+			self.m_arrStack = [];
+
+			// empty current map
+			while ( 0 < $('stones_tbody').childNodes.length ) {
+				$('stones_tbody').removeChild($('stones_tbody').firstChild);
+			}
+
+			// save map
+			self.m_iStones = 0;
+			r.each(rv.map, function(row, y) {
+				var nr = $('stones_tbody').insertRow($('stones_tbody').rows.length);
+				for ( var x=0; x<row.length; x++ ) {
+					var nc = nr.insertCell(nr.cells.length);
+					nc.innerHTML = '';
+					if ( 'o' == row.substr(x, 1) ) {
+						nc.className = 'no-stone';
+						nc.available = true;
+					}
+					else if ( 's' == row.substr(x, 1) ) {
+						nc.className = 'stone';
+						nc.available = true;
+						nc.stone = true;
+						self.m_iStones++;
+					}
+					else {
+						nc.className = '';
+					}
+				}
+			});
+			$('stats_stonesleft').innerHTML = self.m_iStones;
+			// show jumper
+			$('stones_tbody').rows[rv.jumper[1]].cells[rv.jumper[0]].className = 'jumper';
+		});
 		return false;
 	},
 
 	changeName : function( f_szNewName ) {
 		if ( f_szNewName ) {
 			this.m_szName = f_szNewName;
-			$('your_name').innerHTML = f_szNewName;
+			$('your_name').setText(f_szNewName);
 		}
 	},
 
@@ -236,15 +240,12 @@ SteppingStones.prototype = {
 		$('stack_message').style.backgroundColor = 'red';
 		setTimeout("$('stack_message').style.backgroundColor = '';", 500);
 	}
-};
-//-->
+});
 </script>
 </head>
 
 <body>
 <img id="loading" alt="loading" src="images/loading.gif" />
-
-<script type="text/javascript">if ( window.console && 'object' == typeof window.console && window.console.firebug ) { document.write('<div style="background-color:pink;font-weight:bold;margin:10px;padding:10px;color:white;">Firebug can slow this page down... It\'s not necessary but advised to shut it down.</div>'); }</script>
 
 <table border="1" cellpadding="15" cellspacing="0">
 <tr>
@@ -281,43 +282,43 @@ SteppingStones.prototype = {
 </tr>
 </table>
 
-<script type="text/javascript">
-<!--//
-var objStones = new SteppingStones('<?php echo $g_iFirstLevel; ?>');
-
-Ajax.setGlobalHandlers({
-	onStart : function() {
-		$('loading').style.visibility = 'visible';
-	},
-	onComplete: function() {
-		if( !Ajax.busy ) {
-			$('loading').style.visibility = 'hidden';
-		}
+<script>
+r.xhr.busy = 0;
+window.on('xhrStart', function(e) {
+	r.xhr.busy++;
+	$('loading').css('visibility', 'visible');
+});
+window.on('xhrDone', function(e) {
+	if (--r.xhr.busy == 0) {
+		$('loading').css('visibility', 'hidden');
 	}
 });
 
-$('stones_tbody').addEvent('click', function(e) {
-	e = new Event(e).stop();
-	if ( 'TD' !== e.target.nodeName ) { return false; }
-	if ( e.target.stone && 'stone' === e.target.className ) {
+var objStones = new SteppingStones('<?php echo $g_iFirstLevel; ?>');
+
+$('stones_tbody').on('click', function(e) {
+	e.preventDefault();
+
+	if ( e.target.nodeName != 'TD' ) return;
+
+	if ( e.target.stone && e.target.className == 'stone' ) {
 		objStones.selectStone(e.target);
 	}
 });
 
-document.addEvent('keydown', function(e) {
-	e = new Event(e);
-	var dir;
-	switch ( e.code ) {
-		case 37:	dir = 'left';	break;
-		case 38:	dir = 'up';		break;
-		case 39:	dir = 'right';	break;
-		case 40:	dir = 'down';	break;
-		default:	return;			break;
-	}
-	e.stop();
+document.on('keydown', function(e) {
+	var dirs = {
+		"37": 'left',
+		"38": 'up',
+		"39": 'right',
+		"40": 'down',
+	};
+	if ( !dirs[e.key] ) return;
+	var dir = dirs[e.key];
+
+	e.preventDefault();
 	objStones.jump(dir);
 });
-//-->
 </script>
 </body>
 
@@ -334,5 +335,3 @@ function coordsByDir( $f_start, $f_dir ) {
 	}
 	return $to;
 }
-
-?>
