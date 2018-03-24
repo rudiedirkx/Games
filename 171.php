9a0b6ad80e097fb7e9ef95fbf356b5bae64f8922
Rudie Dirkx <github@hotblocks.nl>
2018-03-24 17:01:07 +0100

    traffic+

--- 171.php
+++ 171.php
@@ -1,6 +1,11 @@
 <?php
 // TRAFFIC
 
+require __DIR__ . '/inc.bootstrap.php';
+
+// - turn is 2 actions (position & direction), not 1
+// - wait for room instead of overlap
+
 ?>
 <!doctype html>
 <html>
@@ -12,11 +17,12 @@ canvas {
 	border: solid 1px black;
 }
 </style>
+<script src="<?= html_asset('js/rjs-custom.js') ?>"></script>
 </head>
 
-<body xonload="init()">
+<body>
 
-<canvas width="403" height="403"></canvas>
+<canvas></canvas>
 
 <div id="cars"></div>
 
@@ -31,183 +37,242 @@ var D = {n: [-25, -50, 50, 25], e: [25, -25, 25, 50], s: [-25, 25, 50, 25], w: [
 var O = {n: [0, -1], e: [1, 0], s: [0, 1], w: [-1, 0]};
 var OP = {n: 's', s: 'n', e: 'w', w: 'e'};
 
+r.extend(Coords2D, {
+	rotate: function(angle) {
+		var x = Math.cos(angle) * this.x - Math.sin(angle) * this.y;
+		var y = Math.sin(angle) * this.x + Math.cos(angle) * this.y;
+		return new Coords2D(Math.round(x * 10)/10, Math.round(y * 10)/10);
+	}
+});
 
+class CarShape {
+	constructor(points) {
+		this.points = points || [
+			new Coords2D( 0, -2.5),
+			new Coords2D( 2, -1.5),
+			new Coords2D( 2,  2.5),
+			new Coords2D(-2,  2.5),
+			new Coords2D(-2, -1.5),
+		];
+	}
+
+	rotate(angle) {
+		return new CarShape(this.points.map((C) => C.rotate(angle)));
+	}
+}
+
+class Square {
+	constructor(dirs) {
+		this.dirs = dirs;
+	}
+	get length() {
+		return this.dirs.length;
+	}
+	random() {
+		return this.dirs[Math.floor(Math.random() * this.length)];
+	}
+	includes(dir) {
+		return this.dirs.includes(dir);
+	}
+	[Symbol.iterator]() {
+		return this.dirs[Symbol.iterator]();
+	}
+}
 
 var GRID = [
 	[
-		'es',
-		'esw',
-		'esw',
-		'swe',
+		new Square('es'),
+		new Square('esw'),
+		new Square('esw'),
+		new Square('sw'), // swe
 	],
 	[
-		'nes',
-		'nesw',
-		'nsw',
-		'n',
+		new Square('nes'),
+		new Square('nesw'),
+		new Square('nsw'),
+		new Square('n'),
 	],
 	[
-		'ns',
-		'ns',
-		'nes',
-		'sw',
+		new Square('ns'),
+		new Square('ns'),
+		new Square('nes'),
+		new Square('sw'),
 	],
 	[
-		'new',
-		'new',
-		'new',
-		'nw',
+		new Square('ne'), // new
+		new Square('new'),
+		new Square('new'),
+		new Square('nw'),
 	],
 ];
 
 
 
-function Car(grid, direction, position) {
-	this.grid = grid || [0, 0]; // 0,0 - 3,3
-	this.direction = direction || 'e'; // nesw
-	this.position = position || 0; // 0 - 3
-	this.nextMoves = [];
-}
-Car.goLeft = function(dir) {
-	var di = 'nesw'.indexOf(dir);
-	return di == 0 ? 'w' : 'nesw'[di-1];
-};
-Car.goRight = function(dir) {
-	var di = 'nesw'.indexOf(dir);
-	return di == 3 ? 'n' : 'nesw'[di+1];
-};
-Car.prototype.move = function() {
-	// Pre-defined move
-	if ( this.nextMoves.length ) {
-		var nextMove = this.nextMoves.shift();
-
-		// Move forward
-		if ( nextMove == 'position' ) {
-			this.position++;
-		}
-		else {
-			nextMove.direction && (this.direction = nextMove.direction);
-			nextMove.position && (this.position = nextMove.position);
-		}
+class Car {
+	constructor(name, grid, direction, position) {
+		this.name = name;
+		this.grid = grid; // 0,0 - 3,3
+		this.direction = direction; // nesw
+		this.position = position; // 0 - 3
+		this.nextMoves = [];
+		this.nextDirections = [];
 	}
-	// Pick a direction
-	else if ( this.position == 1 ) {
-		var dir = this.chooseDirection();
-		this.assignNextMoves(dir);
 
-// console.log(dir, JSON.stringify(this.nextMoves, ''));
-		this.move();
+	static goLeft(dir) {
+		var di = 'nesw'.indexOf(dir);
+		return di == 0 ? 'w' : 'nesw'[di-1];
 	}
-	else {
-		this.position++;
 
-		// Advance a square
-		if ( this.position == 4 ) {
-			var nextSquare = this.nextSquare();
-			if ( nextSquare ) {
-				this.position = 0;
-				this.grid = nextSquare;
+	static goRight(dir) {
+		var di = 'nesw'.indexOf(dir);
+		return di == 3 ? 'n' : 'nesw'[di+1];
+	}
+
+	move() {
+		// Pre-defined move
+		if ( this.nextMoves.length ) {
+			var nextMove = this.nextMoves.shift();
+
+			// Move forward
+			if ( nextMove == 'position' ) {
+				this.position++;
 			}
 			else {
-				// Remove this car from the grid
-				var ci = cars.indexOf(this);
-				cars.splice(ci, 1);
+				nextMove.direction && (this.direction = nextMove.direction);
+				nextMove.position && (this.position = nextMove.position);
+			}
+		}
+		// Pick a direction
+		else if ( this.position == 1 ) {
+			var dir = this.chooseDirection();
+			this.assignNextMoves(dir);
+
+	// console.log(dir, JSON.stringify(this.nextMoves, ''));
+			this.move();
+		}
+		else {
+			this.position++;
+
+			// Advance a square
+			if ( this.position == 4 ) {
+				var nextSquare = this.nextSquare();
+				if ( nextSquare ) {
+					this.position = 0;
+					this.grid = nextSquare;
+				}
+				else {
+					// Remove this car from the grid
+					var ci = cars.indexOf(this);
+					cars.splice(ci, 1);
+				}
 			}
 		}
 	}
-};
-Car.prototype.chooseDirection = function() {
-	var grid = GRID[this.grid[1]][this.grid[0]],
-		dir = grid[Math.floor(Math.random() * grid.length)];
 
-	var uturn = dir == OP[this.direction];
-	if ( uturn && !(grid.length == 1 || grid.length == 4) ) {
-		return this.chooseDirection();
+	chooseDirection() {
+		var grid = GRID[this.grid[1]][this.grid[0]];
+		var dir = this.nextDirections.shift() || grid.random();
+
+		var uturn = dir == OP[this.direction];
+		if ( uturn && grid.length > 1 ) {
+			return this.chooseDirection();
+		}
+
+		return dir;
 	}
 
-	return dir;
-};
-Car.prototype.assignNextMoves = function(dir) {
-	// If straight ahead, don't do anything
-	if ( dir == this.direction ) {
-		return this.nextMoves.push('position');
+	assignNextMoves(dir) {
+		// If straight ahead, don't do anything
+		if ( dir == this.direction ) {
+			return this.nextMoves.push('position');
+		}
+
+		// u = U turn
+		// l = left
+		// r = right
+		var di = 'nesw'.indexOf(this.direction);
+		var left = di == 0 ? 'w' : 'nesw'[di-1];
+		var turn = OP[this.direction] == dir ? 'u' : ( left == dir ? 'l' : 'r' );
+
+		switch ( turn ) {
+			case 'u':
+				this.nextMoves.push('position');
+				var d2 = Car.goLeft(this.direction);
+				this.nextMoves.push({direction: d2, position: 2});
+				// this.nextMoves.push('position');
+				var d3 = Car.goLeft(d2);
+				this.nextMoves.push({direction: d3, position: 2});
+				this.nextMoves.push('position');
+				break;
+
+			case 'l':
+				this.nextMoves.push('position');
+				var d2 = Car.goLeft(this.direction);
+				this.nextMoves.push({direction: d2, position: 2});
+				this.nextMoves.push('position');
+				break;
+
+			case 'r':
+				var d2 = Car.goRight(this.direction);
+				this.nextMoves.push({direction: d2, position: 3});
+				// this.nextMoves.push('position');
+				break;
+		}
 	}
 
-	// u = U turn
-	// l = left
-	// r = right
-	var di = 'nesw'.indexOf(this.direction),
-		left = di == 0 ? 'w' : 'nesw'[di-1],
-		turn = OP[this.direction] == dir ? 'u' : ( left == dir ? 'l' : 'r' );
+	nextSquare() {
+		var nextGrid = JSON.parse(JSON.stringify(this.grid));
+		nextGrid[0] += O[this.direction][0];
+		nextGrid[1] += O[this.direction][1];
+		var nextSquare = GRID[nextGrid[1]] && GRID[nextGrid[1]][nextGrid[0]];
+		if ( !nextSquare || !nextSquare.includes(OP[this.direction]) ) {
+			return;
+		}
 
-	switch ( turn ) {
-		case 'u':
-			this.nextMoves.push('position');
-			var d2 = Car.goLeft(this.direction);
-			this.nextMoves.push({direction: d2, position: 2});
-			// this.nextMoves.push('position');
-			var d3 = Car.goLeft(d2);
-			this.nextMoves.push({direction: d3, position: 2});
-			this.nextMoves.push('position');
-			break;
-
-		case 'l':
-			this.nextMoves.push('position');
-			var d2 = Car.goLeft(this.direction);
-			this.nextMoves.push({direction: d2, position: 2});
-			this.nextMoves.push('position');
-			break;
-
-		case 'r':
-			var d2 = Car.goRight(this.direction);
-			this.nextMoves.push({direction: d2, position: 3});
-			// this.nextMoves.push('position');
-			break;
-	}
-};
-Car.prototype.nextSquare = function() {
-	var nextGrid = JSON.parse(JSON.stringify(this.grid));
-	nextGrid[0] += O[this.direction][0];
-	nextGrid[1] += O[this.direction][1];
-	var nextSquare = GRID[nextGrid[1]] && GRID[nextGrid[1]][nextGrid[0]];
-	if ( !nextSquare || nextSquare.indexOf(OP[this.direction]) == -1 ) {
-		return;
+		return nextGrid;
 	}
 
-	return nextGrid;
-};
-Car.prototype.draw = function() {
-	var hor = this.direction == 'e' || this.direction == 'w',
-		w = hor ? 15 : 9,
-		h = hor ? 9 : 15;
+	draw() {
+		var hor = this.direction == 'e' || this.direction == 'w';
+		// var w = hor ? 15 : 9;
+		// var h = hor ? 9 : 15;
 
-	var sx = this.grid[0] * 101,
-		sy = this.grid[1] * 101;
+		var carCenter = {
+			x: this.grid[0] * 101,
+			y: this.grid[1] * 101,
+		};
 
-	var d = this.direction == 'n' || this.direction == 'w' ? -1 : 1,
-		s = d == 1 ? 0 : 100;
+		var dirMoves = this.direction == 'n' || this.direction == 'w' ? -1 : 1;
+		var dirStart = dirMoves == 1 ? 0 : 100;
 
-	var sp = this.direction == 'w' || this.direction == 's' ? 25 : 50;
+		var forwardAxis = hor ? 'x' : 'y';
+		var sidewayAxis = hor ? 'y' : 'x';
 
-	var p = {x: sx, y: sy},
-		a1 = hor ? 'x' : 'y',
-		a2 = hor ? 'y' : 'x';
+		var sidewayPosition = ['s', 'w'].includes(this.direction) ? 0 : 1;
 
-	p[a1] += s + this.position * 25 * d + 5 * d;
-	if ( d == -1 ) {
-		p[a1] -= hor ? w: h;
+		carCenter[forwardAxis] += dirStart + this.position * dirMoves * 25 + dirMoves * 12.5;
+		carCenter[sidewayAxis] += 37.5 + sidewayPosition * 25;
+
+		var directions = ['n', 'e', 's', 'w'];
+		var carshape = draw.carshape.rotate(Math.PI/2 * directions.indexOf(this.direction));
+		ctx.fillStyle = 'red';
+		ctx.beginPath();
+		carshape.points.forEach((point, i) => {
+			ctx[i == 0 ? 'moveTo' : 'lineTo'](carCenter.x + point.x * 3, carCenter.y + point.y * 3);
+		});
+		ctx.closePath();
+		ctx.fill();
+
+		ctx.fillStyle = 'white';
+		ctx.font = '13px sans-serif';
+		ctx.fillText(this.name, carCenter.x-3, carCenter.y+4);
 	}
-
-	p[a2] += sp + 5 + 3;
-
-	ctx.fillStyle = 'red';
-	ctx.fillRect(p.x, p.y, w, h);
-};
-
+}
 
 
 var draw = {
-	line: function(from, to, color, width) {
+	carshape: new CarShape(),
+	line(from, to, color, width) {
 		ctx.lineWidth = width;
 		ctx.strokeStyle = color;
 		ctx.beginPath();
@@ -215,13 +280,16 @@ var draw = {
 		ctx.lineTo(to.x, to.y);
 		ctx.stroke();
 	},
-	structure: function() {
+	structure() {
+		$canvas.width = GRID[0].length * 101 - 1;
+		$canvas.height = GRID.length * 101 - 1;
+
 		GRID.forEach(function(line, y) {
 			line.forEach(function(square, x) {
-				var sx = x * 101,
-					sy = y * 101,
-					ex = sx + 100,
-					ey = sy + 100;
+				var sx = x * 101;
+				var sy = y * 101;
+				var ex = sx + 100;
+				var ey = sy + 100;
 
 				// Square background
 				ctx.fillStyle = 'green';
@@ -233,12 +301,11 @@ var draw = {
 
 				// Side streets
 				var sides = {};
-				for ( var dir, i=0; i<square.length; i++ ) {
-					dir = square[i]
+				for ( var dir of square ) {
 					sides[dir] = 1;
 
-					var left = 50 + D[dir][0],
-						top = 50 + D[dir][1];
+					var left = 50 + D[dir][0];
+					var top = 50 + D[dir][1];
 					ctx.fillRect(sx + left, sy + top, D[dir][2], D[dir][3]);
 				}
 
@@ -255,52 +322,46 @@ var draw = {
 			});
 		});
 	},
-	cars: function() {
+	cars() {
 		cars.forEach(function(car) {
 			car.draw();
 		});
 	},
-	redraw: function() {
+	redraw() {
+		// console.time('redraw');
 		draw.structure();
 		draw.cars();
+		// console.timeEnd('redraw');
 	}
 };
 
 
 
-var car1 = new Car([0, 0], 's', 1);
-// console.log(car1);
-car1.draw();
+var change = true;
 
-var car2 = new Car([0, 1], 'w', 1);
-// console.log(car2);
-car2.draw();
+var cars = [];
+cars.push(new Car(1, [0, 0], 'w', 0));
+cars.push(new Car(2, [0, 1], 'w', 1));
+cars.push(new Car(3, [0, 2], 'n', 1));
+cars.push(new Car(4, [0, 3], 'e', 0));
 
-var car3 = new Car([0, 2], 'n', 1);
-// console.log(car3);
-car3.draw();
-
-var car4 = new Car([0, 3], 'e', 0);
-// console.log(car4);
-car4.draw();
-
-
-
-cars = [car1, car2, car3, car4];
+// First tick will collide the next 2 cars
+cars.push(new Car(5, [2, 0], 'n', 1));
+cars[cars.length-1].nextDirections.push('w');
+cars.push(new Car(6, [2, 0], 'w', 0));
 
 function addCarButton(label, onclick) {
 	var btn = document.createElement('button');
 	btn.textContent = label;
 	btn.onclick = onclick;
-	$cars.appendChild(btn);
+	$cars.append(btn);
+	$cars.append(' ');
 	return btn;
 }
 
 function moveAllCars() {
-	cars.forEach(function(car) {
-		car.move();
-	});
-	draw.redraw();
+	cars.forEach((car) => car.move());
+	change = true;
 }
 
 addCarButton('All', function(e) {
@@ -314,23 +375,26 @@ addCarButton('Start/stop', function(e) {
 		timer = 0;
 	}
 	else {
-		timer = setInterval(function() {
-			moveAllCars();
-		}, 200);
+		timer = setInterval(() => moveAllCars(), 200);
 		moveAllCars();
 	}
-}).click();
+});//.click();
 
 cars.forEach(function(car, i) {
-	addCarButton('Car ' + (i+1), function(e) {
+	addCarButton('Car ' + (i+1), (e) => {
 		car.move();
-		draw.redraw();
+		change = true;
 	});
 });
 
 
 
-draw.redraw();
+function keepDrawing() {
+	change && draw.redraw();
+	change = false;
+	requestAnimationFrame(keepDrawing);
+}
+keepDrawing();
 </script>
 
 </body>
