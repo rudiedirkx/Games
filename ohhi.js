425dcd29c3a785acd40d3dcc34082aa4c5a79c7b
Rudie Dirkx <github@hotblocks.nl>
2020-03-02 00:33:14 +0100

    +0hh1 playable check

--- ohhi.js
+++ ohhi.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class Ohhi extends GridGame {
 
 	constructor( gridElement ) {
@@ -176,7 +178,13 @@ class Ohhi extends GridGame {
 
 		// this.m_objGrid.setHTML(this.createMapHtml(grid));
 
-		const playableGrid = this.hideCellsPlayable(grid);
+		console.time('make playable');
+		var playableGrid;
+		while (!this.isPlayable(playableGrid = this.hideCellsPlayable(grid))) {
+			// redo
+		}
+		console.timeEnd('make playable');
+
 		this.m_objGrid.setHTML(this.createMapHtml(playableGrid));
 	}
 
@@ -184,8 +192,24 @@ class Ohhi extends GridGame {
 		console.log(grid.map(row => row.map(val => val === null ? '_' : Number(val)).join(' ')).join("\n"));
 	}
 
+	isPlayable(grid) {
+		const solver = new OhhiSolver(grid);
+		while (true) {
+			const founds = Array.from(solver.findMustBes());
+			for (let found of founds) {
+				solver.grid[found.y][found.x] = Number(found.color);
+			}
+
+			if (!founds.length) {
+				return solver.isPlayable();
+			}
+		}
+
+		return true;
+	}
+
 	hideCellsPlayable(grid) {
-		return grid.map(cells => cells.map(value => Math.random() > 0.6 ? value : null));
+		return grid.map(cells => cells.map(value => Math.random() < 0.3 ? value : null));
 	}
 
 	restartRow(cells) {
@@ -305,14 +329,14 @@ class Ohhi extends GridGame {
 
 		$('#newgame').on('click', e => {
 			const size = this.m_objGrid.getElements('tr').length;
-			this.createMap(size);
+			requestIdleCallback(() => this.createMap(size));
 		});
 
 		$('#sizes').on('click', 'a[data-size]', e => {
 			e.preventDefault();
 
 			const size = Number(e.target.data('size'));
-			this.createMap(size);
+			requestIdleCallback(() => this.createMap(size));
 		});
 
 		$('#cheat').on('click', e => {
@@ -360,11 +384,23 @@ class OhhiSolver {
 		return new this(grid);
 	}
 
+	static makeCoords(grid) {
+		return grid.map((cells, y) => cells.map((val, x) => new Coords2D(x, y))).flat(1);
+	}
+
 	constructor(grid) {
 		this.size = grid.length;
-		this.grid = grid;
+		this.grid = grid.map(cells => cells.map(val => val == null ? null : Number(val)));
 
-		this.threeStarts = this.grid.map((cells, y) => cells.map((val, x) => new Coords2D(x, y))).flat(1);
+		this.threeStarts = this.constructor.makeCoords(this.grid);
+	}
+
+	getUnknowns() {
+		return this.threeStarts.map(C => this.grid[C.y][C.x]).filter(val => val == null).length;
+	}
+
+	isPlayable() {
+		return this.getUnknowns() == 0;
 	}
 
 	findMustBe() {
@@ -471,7 +507,6 @@ class OhhiSolver {
 				for ( let y2 = 0; y2 < rows.length; y2++ ) {
 					const match = rows[y2].match(re);
 					if (match) {
-console.log('found row match', y);
 						const x1 = line.indexOf('_');
 						const x2 = line.indexOf('_', x1+1);
 						yield this.coordWithColor(x1, y, Number(!Number(match[1])));
@@ -489,7 +524,6 @@ console.log('found row match', y);
 				for ( let x2 = 0; x2 < cols.length; x2++ ) {
 					const match = cols[x2].match(re);
 					if (match) {
-console.log('found col match', x);
 						const y1 = line.indexOf('_');
 						const y2 = line.indexOf('_', y1+1);
 						yield this.coordWithColor(x, y1, Number(!Number(match[1])));
