2d9435e54dcf1b800232d816604b79e28a7164c8
Rudie Dirkx <github@hotblocks.nl>
2020-03-13 19:38:27 +0100

    +blackbox in GrigGame

---
+++ 187.php
@@ -0,0 +1,39 @@
+<?php
+// BLACKBOX (JS)
+
+require __DIR__ . '/inc.bootstrap.php';
+
+?>
+<!doctype html>
+<html>
+
+<head>
+<meta charset="utf-8" />
+<title>Blackbox</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="blackbox.css" />
+<? include 'tpl.onerror.php' ?>
+<script src="<?= html_asset('js/rjs-custom.js') ?>"></script>
+<script src="<?= html_asset('gridgame.js') ?>"></script>
+<script src="<?= html_asset('blackbox.js') ?>"></script>
+</head>
+
+<body>
+
+<table id="blackbox"></table>
+
+<p style="text-align: center">
+	<span id="stats"></span>
+</p>
+<p style="text-align: center">
+	<button id="newgame">New game</button>
+</p>
+
+<script>
+objGame = new Blackbox($('#blackbox'));
+objGame.createMap();
+objGame.listenControls();
+</script>
+</body>
+
+</html>
---
+++ blackbox.js
@@ -0,0 +1,202 @@
+class Blackbox extends GridGame {
+
+	static SIZE = 8;
+	static SIDES = ['top', 'right', 'bottom', 'left'];
+
+	reset() {
+		super.reset();
+
+		this.atoms = 5;
+		this.checker = 0;
+	}
+
+	resetMap() {
+		this.m_objGrid.removeClass('show-atoms');
+		this.m_objGrid.getElements('td[data-side]').attr('style', null);
+		this.m_objGrid.getElements('td.grid').removeClass('atom').removeClass('hilite');
+	}
+
+	createMap() {
+		this.reset();
+		this.resetMap();
+
+		const coords = this.makeAllCoords();
+		coords.sort(() => Math.random() > 0.5 ? 1 : -1);
+		coords.slice(0, this.atoms).forEach(C => this.m_objGrid.rows[C.y].cells[C.x].addClass('atom'));
+
+		// this.m_objGrid.addClass('show-atoms');
+	}
+
+	makeAllCoords() {
+		return this.m_objGrid.getElements('.grid').map(cell => this.getCoord(cell));
+	}
+
+	haveWon() {
+		return this.m_objGrid.getElements('.atom:not(.hilite)').length == 0 && this.m_objGrid.getElements('.hilite:not(.atom)').length == 0;
+	}
+
+	win() {
+		this.m_objGrid.addClass('show-atoms');
+
+		super.win();
+	}
+
+	randomColor() {
+		return '#' + ('000000' + (Math.random()*0xFFFFFF<<0).toString(16)).slice(-6);
+	}
+
+	getNextMove(cur) {
+		const next = this.getNext(cur.loc, cur.dir);
+		const left = this.getLeft(next, cur.dir);
+		const right = this.getRight(next, cur.dir);
+
+		const atomAhead = next.hasClass('atom');
+
+		if (atomAhead) {
+			// Absorbed
+			return false;
+		}
+
+		const atomLeft = left.hasClass('atom');
+		const atomRight = right.hasClass('atom');
+
+		if (atomLeft && atomRight) {
+			// Back to source
+			return true;
+		}
+
+		if (atomLeft) {
+			// Turn right
+			return new BlackboxMove(cur.loc, (cur.dir + 1) % 4);
+		}
+
+		if (atomRight) {
+			// Turn left
+			return new BlackboxMove(cur.loc, (cur.dir + 3) % 4);
+		}
+
+		// Straight ahead
+		return new BlackboxMove(next, cur.dir);
+	}
+
+	getCoord(cell) {
+		return new Coords2D(cell.cellIndex, cell.parentNode.rowIndex);
+	}
+
+	getNext(cell, dir) {
+		const C = this.getCoord(cell);
+		const d = this.dir4Coords[dir];
+		const next = this.m_objGrid.rows[C.y+d.y].cells[C.x+d.x];
+		return next;
+	}
+
+	getLeft(cell, dir) {
+		return this.getNext(cell, (dir + 3) % 4);
+	}
+
+	getRight(cell, dir) {
+		return this.getNext(cell, (dir + 1) % 4);
+	}
+
+	finishMove(start, end) {
+		start.style.backgroundColor = end.style.backgroundColor = this.randomColor();
+	}
+
+	finishMoveAbsorbed(start) {
+		start.style.backgroundColor = 'gray';
+	}
+
+	finishMoveSource(start) {
+		start.style.backgroundColor = 'white';
+	}
+
+	handleSideClick(cell) {
+		this.setMoves(this.m_iMoves + 1);
+
+		let cur = new BlackboxMove(
+			cell,
+			(this.constructor.SIDES.indexOf(cell.data('side')) + 2) % 4
+		);
+		for ( let i = 0; i < 200; i++ ) {
+			cur = this.getNextMove(cur);
+
+			if (cur === false) {
+				this.finishMoveAbsorbed(cell);
+				break;
+			}
+			if (cur === true || cur.loc === cell) {
+				this.finishMoveSource(cell);
+				break;
+			}
+			if (cur.loc.data('side')) {
+				this.finishMove(cell, cur.loc);
+				break;
+			}
+		}
+	}
+
+	handleInsideClick(cell) {
+		cell.toggleClass('hilite');
+
+		clearTimeout(this.checker);
+		this.checker = setTimeout(() => this.winOrLose(), 500);
+	}
+
+	handleCellClick(cell) {
+		if (this.m_bGameOver) {
+			return this.createMap();
+		}
+
+		this.startTime();
+
+		if (cell.data('side')) {
+			this.handleSideClick(cell);
+		}
+		else if (cell.hasClass('grid')) {
+			this.handleInsideClick(cell);
+		}
+	}
+
+	listenControls() {
+		this.listenCellClick();
+
+		$('#newgame').on('click', e => {
+			this.createMap();
+		});
+	}
+
+	createGame() {
+		this.createStats();
+
+		this.createBoard();
+	}
+
+	createBoard() {
+		const corner = '<td class="corner"></td>';
+		const top = '<td data-side="top"></td>';
+		const left = '<td data-side="left"></td>';
+		const right = '<td data-side="right"></td>';
+		const bottom = '<td data-side="bottom"></td>';
+		const inside = '<td class="grid"></td>';
+		const html = '' +
+			`<tr>${corner}${top.repeat(this.constructor.SIZE)}${corner}</tr>\n` +
+			`<tr>${left}${inside.repeat(this.constructor.SIZE)}${right}</tr>\n`.repeat(this.constructor.SIZE) +
+			`<tr>${corner}${bottom.repeat(this.constructor.SIZE)}${corner}</tr>\n`;
+
+		$('table').setHTML(html);
+	}
+
+	getStatsDelimiter() {
+		return ' | ';
+	}
+
+}
+
+class BlackboxMove {
+
+	constructor(loc, dir) {
+		this.loc = loc;
+		this.dir = dir;
+	}
+
+}
