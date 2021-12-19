0e5cc3a6f155f5f2779a2cdcbe5600c659fa2ca3
Rudie Dirkx <github@hotblocks.nl>
2021-12-19 20:45:25 +0100

    mondrian in gridgame

--- 174.php
+++ 174.php
@@ -16,8 +16,17 @@ require __DIR__ . '/inc.bootstrap.php';
 <script src="<?= html_asset('mondrian.js') ?>"></script>
 <title>MONDRIAN PUZZLE</title>
 <style>
+html, body {
+	margin: 0;
+	padding: 0;
+}
+body {
+	margin: 5px;
+}
 canvas {
 	outline: solid 1px black;
+	max-width: calc(100vw - 10px);
+	max-height: calc(100vh - 10px);
 	max-width: 100%;
 	touch-action: none;
 }
@@ -29,11 +38,10 @@ canvas {
 </head>
 
 <body>
-
-<canvas width="100" height="100"></canvas>
+<canvas></canvas>
 
 <p>
-	Click &amp; drag to draw rectangles.
+	Click &amp; drag rectangles to fill the board.
 	Size: <select id="size"><?= do_html_options(array_combine(range(4, 9), range(4, 9))) ?></select>
 	<button id="reset">Reset</button>
 	<button id="undo">Undo</button>
@@ -42,260 +50,11 @@ canvas {
 <p>Score: <code id="score">?</code> (the lower the better)</p>
 
 <script>
-var sizeElement = document.querySelector('#size');
-var resetElement = document.querySelector('#reset');
-var undoElement = document.querySelector('#undo');
-var scoreElement = document.querySelector('#score');
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-
-var SQUARE_SIZE = 80;
-var BOARD_SIZE = 5;
-var BOARD_MARGIN = 20;
-var COLORS = ['#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff'];
-
-var start = Date.now();
-var squares = [];
-var squaring = [];
-var error;
-var change = true;
-var drawing = false;
-
-function drawSquare(square, color, number) {
-	// Outer lines
-	var rect = square.rect();
-	drawLines(rect.concat(rect[0]), 3, color);
-
-	// Inner fill
-	ctx.fillStyle = color == 'error' ? 'black' : color.replace(/0/g, 'b');
-	var from = square.from.rect();
-	var to = square.to.rect();
-	ctx.fillRect(from.x, from.y, to.x - from.x, to.y - from.y);
-
-	// Number
-	if (number) {
-		drawSquareNumber(square, color, number);
-	}
-}
-
-function drawSquareNumber(square, color, number) {
-	// number || (number = square.coverage());
-	var center = square.center();
-
-	ctx.font = '60px sans-serif';
-	ctx.fillStyle = color == 'error' ? 'white' : color;
-	ctx.textAlign = 'center';
-	ctx.textBaseline = 'middle';
-	ctx.fillText(String(number), center.x, center.y);
-}
-
-function drawLines(ps, width, color) {
-	ctx.strokeStyle = color;
-	ctx.lineWidth = width;
-
-	ctx.beginPath();
-	ctx.moveTo(ps[0].x, ps[0].y);
-	for (var i = 1; i < ps.length; i++) {
-		ctx.lineTo(ps[i].x, ps[i].y);
-	}
-	ctx.closePath();
-	ctx.stroke();
-}
-
-function drawLine(p1, p2, width, color) {
-	drawLines([p1, p2], width, color);
-}
-
-function drawPoint(point) {
-	ctx.fillStyle = 'black';
-	ctx.fillRect(point.x-1, point.y-1, 3, 3);
-}
-
-function drawGrid() {
-	for (var y = BOARD_MARGIN; y < canvas.width; y+=SQUARE_SIZE+1) {
-		drawLine(new Point(0, y), new Point(canvas.width, y), 1, '#ddd');
-	}
-
-	for (var x = BOARD_MARGIN; x < canvas.width; x+=SQUARE_SIZE+1) {
-		drawLine(new Point(x, 0), new Point(x, canvas.height), 1, '#ddd');
-	}
-}
-
-function getColor(i) {
-	return COLORS[i % COLORS.length];
-}
-
-function drawSquares() {
-	for (var i = 0; i < squares.length; i++) {
-		drawSquare(squares[i], getColor(i));
-	}
-}
-
-function drawSquareNumbers() {
-	for (var i = 0; i < squares.length; i++) {
-		drawSquareNumber(squares[i], getColor(i), squares[i].coverage());
-	}
-}
-
-function drawError() {
-	if (error) {
-		drawSquare(error, 'error', 'x');
-	}
-}
-
-function drawSquaring() {
-	for (var i = 1; i < squaring.length; i++) {
-		drawLine(squaring[i-1].rect(), squaring[i].rect(), 2, 'black');
-	}
-}
-
-function updateSize() {
-	canvas.width = canvas.height = BOARD_SIZE * SQUARE_SIZE + BOARD_MARGIN * 2 + BOARD_SIZE + 1;
-}
-
-function finishDrawing() {
-	if (drawing && squaring.length) {
-		var square = Square.valid(squaring);
-		if (square) {
-			if (square.overlaps(squares) || square.sameSizes(squares)) {
-				error = square;
-				setTimeout(function() {
-					error = null;
-					change = true;
-				}, 500);
-			}
-			else {
-				squares.push(square);
-				squaring = [];
-
-				updateScore();
-			}
-		}
-	}
-
-	squaring.length = 0;
-	change = true;
-}
-
-function getScore() {
-	if (squares.length < 2) {
-		return 0;
-	}
-
-	var max = -1;
-	var min = 99;
-
-	for (var i = 0; i < squares.length; i++) {
-		var coverage = squares[i].coverage();
-		max = Math.max(max, coverage);
-		min = Math.min(min, coverage);
-	}
-
-	return max - min;
-}
-
-function getComplete() {
-	return BOARD_SIZE * BOARD_SIZE == squares.reduce(function(area, square) {
-		return area + square.coverage();
-	}, 0);
-}
-
-function updateScore() {
-	var score = getScore();
-	var complete = getComplete();
-	scoreElement.textContent = score ? String(score) : '?';
-	scoreElement.parentNode.classList.toggle('complete', complete);
-
-	if (complete) {
-		Game.saveScore({
-			time: Math.round((Date.now() - start) / 1000),
-			score: score,
-			level: BOARD_SIZE,
-		});
-	}
-}
-
-function reset() {
-	start = Date.now();
-
-	error = null;
-	squares.length = 0;
-	squaring.length = 0;
-
-	updateScore();
-
-	change = true;
-}
-
-// === //
-
-sizeElement.value = String(BOARD_SIZE);
-sizeElement.onchange = function(e) {
-	BOARD_SIZE = Number(this.value);
-	updateSize();
-	reset();
-};
-
-resetElement.onclick = function(e) {
-	reset();
-};
-
-undoElement.onclick = function(e) {
-	squares.pop();
-	change = true;
-	updateScore();
-};
-
-canvas.on(['mousedown', 'touchstart'], function(e) {
-	// e.preventDefault();
-	drawing = true;
-});
-canvas.on(['mousemove', 'touchmove'], function(e) {
-	if (!drawing) return;
-	// e.preventDefault();
-
-	var point = (new Point(e.subjectXY.x, e.subjectXY.y)).factor(canvas.width / canvas.offsetWidth);
-	var intersect = point.findClosestIntersection();
-	if (!squaring.length || squaring[squaring.length-1].validNext(intersect)) {
-		if (!Point.contains(squaring, intersect)) {
-			squaring.push(intersect);
-			change = true;
-		}
-		else if (squaring.length > 1 && squaring[0].equals(intersect)) {
-			finishDrawing();
-			drawing = false;
-		}
-	}
-});
-document.onmouseup = document.ontouchend = function(e) {
-	// e.preventDefault();
-
-	finishDrawing();
-	drawing = false;
-};
-
-// === //
-
-updateSize();
-render();
-
-function render() {
-	if (change) {
-		change = false;
-
-		canvas.width = canvas.width;
-
-		drawSquares();
-		drawGrid();
-		drawError();
-		drawSquaring();
-		drawSquareNumbers();
-	}
-
-	(window.requestAnimationFrame || window.webkitRequestAnimationFrame)(render);
-}
+objGame = new Mondrian(document.querySelector('canvas'));
+objGame.listenControls();
+objGame.startGame();
+objGame.startPainting();
 </script>
-
 </body>
 
 </html>
--- mondrian.js
+++ mondrian.js
@@ -1,165 +1,269 @@
-class Point {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	factor(factor) {
-		this.x *= factor;
-		this.y *= factor;
-		return this;
-	}
-
-	distanceTo(point) {
-		return Math.sqrt(Math.pow(Math.abs(point.x - this.x), 2) + Math.pow(Math.abs(point.y - this.y), 2));
-	}
-
-	findClosestIntersection() {
-		var x = Math.round((this.x - BOARD_MARGIN) / (SQUARE_SIZE + 1));
-		var y = Math.round((this.y - BOARD_MARGIN) / (SQUARE_SIZE + 1));
-		return new Point(x, y);
-	}
-
-	rect() {
-		return new Point(BOARD_MARGIN + this.x * (SQUARE_SIZE+1), BOARD_MARGIN + this.y * (SQUARE_SIZE+1));
-	}
-
-	equals(point) {
-		return this.x == point.x && this.y == point.y;
-	}
-
-	validNext(point) {
-		return Math.abs(point.x - this.x) + Math.abs(point.y - this.y) == 1;
-	}
-
-	static contains(points, point) {
-		for (var i = 0; i < points.length; i++) {
-			if (points[i].equals(point)) {
-				return true;
-			}
-		}
-	}
-}
-
-class Line {
-	constructor(from, to) {
-		this.from = from;
-		this.to = to;
-	}
-
-	equals(line) {
-		return (this.from.equals(line.from) && this.to.equals(line.to)) || (this.from.equals(line.to) && this.to.equals(line.from));
-	}
-}
-
 class Square {
 	constructor(from, to) {
 		this.from = from;
 		this.to = to;
 	}
 
-	rect() {
-		return [
-			this.from.rect(),
-			(new Point(this.to.x, this.from.y)).rect(),
-			this.to.rect(),
-			(new Point(this.from.x, this.to.y)).rect(),
-		];
-	}
-
-	center() {
-		return (new Point(
-			this.from.x + (this.to.x - this.from.x)/2,
-			this.from.y + (this.to.y - this.from.y)/2
-		)).rect();
-	}
-
-	coverage() {
-		return (this.to.x - this.from.x) * (this.to.y - this.from.y);
-	}
-
-	points() {
-		var points = [];
-
-		for (var x = this.from.x; x <= this.to.x; x++) {
-			points.push(new Point(x, this.from.y));
-			points.push(new Point(x, this.to.y));
+	fromTopLeft() {
+		if (this.from.x <= this.to.x && this.from.y <= this.to.y) {
+			return this;
 		}
 
-		for (var y = this.from.y + 1; y <= this.to.y - 1; y++) {
-			points.push(new Point(this.from.x, y));
-			points.push(new Point(this.to.x, y));
-		}
-
-		return points;
-	}
-
-	sameSize(square) {
-		return this.to.x - this.from.x == square.to.x - square.from.x && this.to.y - this.from.y == square.to.y - square.from.y;
-	}
-
-	sameSizes(squares) {
-		for (var i = 0; i < squares.length; i++) {
-			if (squares[i].sameSize(this)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	overlap(square) {
-		return this.from.x < square.to.x && this.to.x > square.from.x && this.from.y < square.to.y && this.to.y > square.from.y;
-	}
-
-	overlaps(squares) {
-		for (var i = 0; i < squares.length; i++) {
-			if (squares[i].overlap(this)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	static bounds(points) {
-		var from = new Point(99, 99);
-		var to = new Point(-1, -1);
-
-		for (var i = 0; i < points.length; i++) {
-			var point = points[i];
-
-			from.x = Math.min(from.x, point.x);
-			from.y = Math.min(from.y, point.y);
-			to.x   = Math.max(to.x  , point.x);
-			to.y   = Math.max(to.y  , point.y);
-		}
+		const flipX = this.from.x > this.to.x;
+		const flipY = this.from.y > this.to.y;
+		const from = new Coords2D(flipX ? this.to.x : this.from.x, flipY ? this.to.y : this.from.y);
+		const to = new Coords2D(flipX ? this.from.x : this.to.x, flipY ? this.from.y : this.to.y);
 
 		return new Square(from, to);
 	}
 
-	static valid(points) {
-		var square = Square.bounds(points);
-		if (square.coverage() == 0) {
-			return false;
-		}
-
-		var squarePoints = square.points();
-		if (squarePoints.length != points.length) {
-			return false;
-		}
-
-		var distance = points[0].distanceTo(points[points.length-1]);
-		if (distance !== 1) {
-			return false;
-		}
-
-		for (var i = 0; i < squarePoints.length; i++) {
-			if (!Point.contains(points, squarePoints[i])) {
-				return false;
-			}
-		}
-
-		return square;
+	area() {
+		return (this.to.x - this.from.x + 1) * (this.to.y - this.from.y + 1);
 	}
+
+	shape() {
+		return `${this.to.x - this.from.x + 1}x${this.to.y - this.from.y + 1}`;
+	}
+
+	center() {
+		return new Coords2D((this.to.x + this.from.x + 1) / 2, (this.to.y + this.from.y + 1) / 2);
+	}
+
+	overlap(S) {
+		return this.from.x < S.to.x + 1 && this.to.x + 1 > S.from.x && this.from.y < S.to.y + 1 && this.to.y + 1 > S.from.y;
+	}
+}
+
+class Mondrian extends CanvasGame {
+
+	static OFFSET = 20;
+	static WHITESPACE = 10;
+	static SQUARE = 80;
+	static COLORS = ['#f00', '#0f0', '#00f', '#cc0', '#f0f', '#0ff'];
+
+	reset() {
+		super.reset();
+
+		// this.paintingTiming = true;
+
+		this.SIZE = 0;
+		this.squares = [];
+		// this.hovering = null;
+		this.dragging = null;
+		this.squaring = null;
+		this.error = null;
+	}
+
+	startGame(size = 5) {
+		this.reset();
+
+		this.SIZE = size;
+		$('#size').value = size;
+
+		this.canvas.width = this.canvas.height = Mondrian.OFFSET * 2 + Mondrian.SQUARE * this.SIZE;
+		this.changed = true;
+
+		this.printScore();
+	}
+
+	getScore() {
+		return {
+			time: this.getTime(),
+			moves: this.squares.length,
+			score: this.getNumberScore(),
+		};
+	}
+
+	getNumberScore() {
+		if (this.squares.length < 2) return 0;
+
+		const areas = this.squares.map(S => S.area());
+		return Math.max(...areas) - Math.min(...areas);
+	}
+
+	printScore() {
+		$('#score').setText(this.getNumberScore() || '?');
+	}
+
+	scale(source) {
+		if (source instanceof Coords2D) {
+			return new Coords2D(this.scale(source.x), this.scale(source.y));
+		}
+
+		return Mondrian.OFFSET + source * Mondrian.SQUARE;
+	}
+
+	unscale(source) {
+		if (source instanceof Coords2D) {
+			source = source.multiply(this.canvas.width / this.canvas.offsetWidth);
+			const C = new Coords2D(this.unscale(source.x), this.unscale(source.y));
+			return this.inside(C) ? C : null;
+		}
+
+		return Math.round((source - Mondrian.OFFSET - Mondrian.SQUARE/2) / Mondrian.SQUARE);
+	}
+
+	inside(coord) {
+		return coord.x >= 0 && coord.x < this.SIZE && coord.y >= 0 && coord.y < this.SIZE;
+	}
+
+	getColor(index) {
+		return Mondrian.COLORS[index % Mondrian.COLORS.length];
+	}
+
+	drawContent() {
+		// this.drawHovering();
+		this.drawSquares();
+		this.drawGrid();
+		this.drawSquareNumbers();
+		this.drawError();
+		this.drawSquaring();
+	}
+
+	// drawHovering() {
+	// 	if (this.hovering) {
+	// 		const C = this.hovering;
+	// 		this.drawRectangle(this.scale(C), this.scale(C.add(new Coords2D(1, 1))), {color: '#eee', fill: true});
+	// 	}
+	// }
+
+	drawSquares() {
+		this.squares.forEach((S, i) => this.drawSquare(S, this.getColor(i) + '5', true));
+	}
+
+	drawError() {
+		if (this.error) {
+			this.drawSquare(this.error, '#000', true);
+			this.drawSquareNumber(this.error.center(), 'x', '#fff');
+		}
+	}
+
+	drawSquaring() {
+		if (this.squaring) {
+			this.drawSquare(this.squaring, '#000', false);
+		}
+	}
+
+	drawSquare(square, color, fill) {
+		this.drawRectangle(
+			this.scale(square.from),
+			this.scale(square.to.add(new Coords2D(1, 1))),
+			{color, fill}
+		);
+	}
+
+	drawSquareNumbers() {
+		this.squares.forEach((S, i) => {
+			this.drawSquareNumber(S.center(), S.area(), this.getColor(i));
+		});
+	}
+
+	drawSquareNumber(C, text, color) {
+		this.ctx.textAlign = 'center';
+		this.ctx.textBaseline = 'middle';
+		this.drawText(this.scale(C), text, {size: 60, color});
+	}
+
+	drawGrid() {
+		for (var y = Mondrian.OFFSET; y < this.canvas.width; y += Mondrian.SQUARE) {
+			this.drawLine(new Coords2D(Mondrian.WHITESPACE, y), new Coords2D(this.canvas.width - Mondrian.WHITESPACE, y), {color: '#ddd', width: 1});
+		}
+
+		for (var x = Mondrian.OFFSET; x < this.canvas.width; x += Mondrian.SQUARE) {
+			this.drawLine(new Coords2D(x, Mondrian.WHITESPACE), new Coords2D(x, this.canvas.height - Mondrian.WHITESPACE), {color: '#ddd', width: 1});
+		}
+	}
+
+	validNewSquare(square) {
+		if (this.squares.map(S => S.shape()).includes(square.shape())) return false;
+		if (this.squares.some(S => S.overlap(square))) return false;
+
+		return true;
+	}
+
+	haveWon() {
+		return this.squares.reduce((T, S) => T + S.area(), 0) == this.SIZE * this.SIZE;
+	}
+
+	handleDragging(C) {
+		C = this.unscale(C);
+		if (!C) return;
+
+		if (!this.squaring) {
+			this.squaring = new Square(C, C);
+		}
+		else {
+			this.squaring = (new Square(this.dragging, C)).fromTopLeft();
+		}
+
+		this.changed = true;
+	}
+
+	handleDragEnd() {
+		if (!this.squaring) return;
+
+		if (this.validNewSquare(this.squaring)) {
+			this.squares.push(this.squaring);
+			this.printScore();
+			this.startWinCheck();
+		}
+		else {
+			this.error = this.squaring;
+			setTimeout(() => {
+				this.error = null;
+				this.changed = true;
+			}, 500);
+		}
+
+		this.squaring = null;
+	}
+
+	listenDrag() {
+		this.canvas.on(['mousedown', 'touchstart'], e => {
+			if (!e.rightClick) {
+				this.dragging = this.unscale(e.subjectXY);
+			}
+		});
+		this.canvas.on(['mousemove', 'touchmove'], e => {
+			if (this.dragging) {
+				this.handleDragging(e.subjectXY);
+			}
+			// else {
+			// 	if (e.type == 'mousemove') {
+			// 		this.hovering = this.unscale(e.subjectXY);
+			// 		this.changed = true;
+			// 	}
+			// }
+		});
+		// this.canvas.on('mouseout', e => {
+		// 	this.hovering = null;
+		// });
+		document.onmouseup = document.ontouchend = e => {
+			// e.preventDefault();
+
+			this.handleDragEnd();
+			// this.hovering = null;
+			this.dragging = null;
+			this.changed = true;
+		};
+	}
+
+	listenControls() {
+		this.listenDrag();
+
+		$('#size').on('change', e => {
+			this.startGame(parseInt(e.target.value));
+		});
+
+		$('#reset').on('click', e => {
+			this.startGame(this.SIZE);
+		});
+
+		$('#undo').on('click', e => {
+			this.squares.pop();
+			this.changed = true;
+			this.printScore();
+		});
+	}
+
 }
