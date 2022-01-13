f851ba2104e900a3c04a36f0391d5d5feb740d66
Rudie Dirkx <github@hotblocks.nl>
2022-01-13 17:10:42 +0100

    abalone mobile lol

--- 143.php
+++ 143.php
@@ -166,6 +166,8 @@ $friendURL = 'https://' . $_SERVER['HTTP_HOST'] . '/143.php?login=' . $objOppone
 <head>
 <title>Abalone</title>
 <link rel="stylesheet" href="143.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta charset="utf-8" />
 </head>
 
 <body>
